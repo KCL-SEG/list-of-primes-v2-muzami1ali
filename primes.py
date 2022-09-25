4e68f5608aafab6cf72d49169af34ca4ccbbabd2
muzami1ali <muzamil.ali@kcl.ac.uk>
2022-09-25 14:46:13 +0100

    Modify primes.py to return list of prime numbers

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes<=0:
+        raise ValueError("Please enter a value greater than zero")
     list = []
+    count = 0
+    i = 2
+    x = 2 
+    while len(list)!=number_of_primes:
+        while x>0:
+            y = i%x
+            if y==0:
+                count +=1
+            x -= 1
+        if count<3:
+            list.append(i)
+        count=0
+        i+=1
+        x = i
     return list
